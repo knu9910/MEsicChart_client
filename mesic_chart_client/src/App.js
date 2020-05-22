5b3eda80700b5b26dd1c77ada81fa06f6887297b
LEEHEEJOON <marco920322@gmail.com>
2020-05-22 13:08:58 +0900

    tmp commit

--- mesic_chart_client/src/App.js
+++ mesic_chart_client/src/App.js
@@ -1,43 +1,47 @@
-import React from 'react';
-import { Switch, Route, Redirect} from 'react-router-dom';
-import Main from './components/Main/Main';
-import MainSearch from './components/Main/MainSearch';
-import RecommandedMusicList from './components/Main/RecommandedMusicList';
-import SignUp from './components/SignUp';
-import SignIn from './components/SignIn';
-import PlayList from './components/PlayList/PlayList'
-import PlayListSearch from './components/PlayList/PlayListSearch';
-import MyMusicList from './components/PlayList/MyMusicList';
-import Footer from './components/Footer';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Main from "./pages/Main";
+import MainSearch from "./components/Main/MainSearch";
+import RecommandedMusicList from "./components/Main/RecommandedMusicList";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import PlayList from "./pages/PlayList";
+import PlayListSearch from "./components/PlayList/PlayListSearch";
+import MyMusicList from "./components/PlayList/MyMusicList";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    
+  constructor(props) {
+    super(props);
+
     this.state = {
-      isSignIn: false
-    }
+      isSignIn: false,
+    };
   }
 
   render() {
+    const { isSignIn } = this.state;
+
     return (
       <div>
         <Switch>
+          <Route exact path="/signup" render={() => <SignUp />} />
           <Route
-          exact path="/signup"
-          render={() => <SignUp/>}
+            exact
+            path="/signin"
+            render={() => <SignIn isSignIn={isSignIn} />}
           />
+          <Route exact path="/playlist" render={() => <PlayList />} />
+          <Route exact path="/" render={() => <Main />} />
           <Route
-          exact path="/signin"
-          render={() => <SignIn />}
+            path="/"
+            render={() => {
+              if (isSignIn) {
+                return <Redirect to="/main" />;
+              }
+              return <Redirect to="/signin" />;
+            }}
           />
-          <Route
-          exact path="/playlist"
-          render={() => <PlayList/>}
-          />
-          <Route 
-          exact path="/"
-          render={() => <Main/>}/>
         </Switch>
       </div>
     );
