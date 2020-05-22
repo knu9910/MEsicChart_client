5c87f55f25a5a9e7da4a2b0e3c8aa1631b067823
LEEHEEJOON <marco920322@gmail.com>
2020-05-22 12:23:36 +0900

    구조 변경

---
+++ mesic_chart_client/src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h2>
+                메인화면
+            </h2>
+        </div>
+    );
+};
+export default Main;
---
+++ mesic_chart_client/src/youtubeApi/searchMusicsByText.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const key = process.env.YOUTUBE_API_KEY;
+
+const fetch = require("node-fetch");
+const urlencode = require("urlencode");
+
+const part = "snippet";
+const type = "video"; // 채널 아닌 비디오만
+const videoCategoryId = "10"; // 음악
+
+const searchMusicsByText = async (text, listCount) => {
+  text = urlencode(text);
+  return fetch(
+    "https://www.googleapis.com/youtube/v3/search?" +
+      `part=${part}&key=${key}&maxResults=${listCount}&q=${text}&type=${type}` +
+      `&videoCategoryId=${videoCategoryId}`
+  );
+};
+
+module.exports = searchMusicsByText;
+
+/*
+사용 예제
+
+searchMusicsByText('여행', 3)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(err));
+*/
