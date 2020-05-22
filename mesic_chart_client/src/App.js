d13e516fb0eb1f886740807c98a107cfdc524807
LHJ <61103879+LEEHEEJOON@users.noreply.github.com>
2020-05-22 12:40:07 +0900

    Revert "디렉토리 구조 변경"

--- mesic_chart_client/src/App.js
+++ mesic_chart_client/src/App.js
@@ -1,47 +1,36 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import Main from "./pages/Main";
-import MainSearch from "./components/Main/MainSearch";
-import RecommandedMusicList from "./components/Main/RecommandedMusicList";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import PlayList from "./pages/PlayList";
-import PlayListSearch from "./components/PlayList/PlayListSearch";
-import MyMusicList from "./components/PlayList/MyMusicList";
-import Footer from "./components/Footer";
+import React from 'react';
+import { Switch, Route, Redirect} from 'react-router-dom';
+import Main from './components/Main/Main';
+import MainSearch from './components/Main/MainSearch';
+import RecommandedMusicList from './components/Main/RecommandedMusicList';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import PlayList from './components/PlayList/PlayList'
+import PlayListSearch from './components/PlayList/PlayListSearch';
+import MyMusicList from './components/PlayList/MyMusicList';
+import Footer from './components/Footer';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isSignIn: false,
-    };
-  }
 
   render() {
-    const { isSignIn } = this.state;
-
     return (
       <div>
         <Switch>
-          <Route exact path="/signup" render={() => <SignUp />} />
           <Route
-            exact
-            path="/signin"
-            render={() => <SignIn isSignIn={isSignIn} />}
+          exact path="/signup"
+          render={() => <SignUp/>}
           />
-          <Route exact path="/playlist" render={() => <PlayList />} />
-          <Route exact path="/" render={() => <Main />} />
           <Route
-            path="/"
-            render={() => {
-              if (isSignIn) {
-                return <Redirect to="/main" />;
-              }
-              return <Redirect to="/signin" />;
-            }}
+          exact path="/signin"
+          render={() => <SignIn/>}
           />
+          <Route
+          exact path="/playlist"
+          render={() => <PlayList/>}
+          />
+          <Route 
+          exact path="/"
+          render={() => <Main/>}/>
         </Switch>
       </div>
     );
---
+++ mesic_chart_client/src/components/PlayList/PlayList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlayList = () => {
+    return (
+        <div>
+            <h2>
+                플레이리스트
+            </h2>
+        </div>
+    );
+};
+export default PlayList;
