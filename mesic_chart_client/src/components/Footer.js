7f1ba156de1def8f7b527f748122e8d731ac155a
Lee-Gwang-soo <59168432+Lee-Gwang-soo@users.noreply.github.com>
2020-05-25 12:55:31 +0900

    메인화면 디자인 구현(JSX,CSS)
     - 하단 Footer 구현
     - 메인화면 css 기본 구현 및 JSX 적용

--- mesic_chart_client/src/components/Footer.js
+++ mesic_chart_client/src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-const footer = () => {
+const footer = (props) => {
+    const currentSign = props.isSignIn ? "로그아웃" : "로그인"
     return (
-        <div>
-            <h2>
-                로그인
-            </h2>
+        <div className="footer">
+            <button className="go-List-Button"><Link to="/PlayList">플레이리스트</Link></button>
+            <button className="signout-Button"><Link to="/SignIn">{currentSign}</Link></button>
         </div>
     );
 };
+
 export default footer;
